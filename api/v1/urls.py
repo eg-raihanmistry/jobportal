68c63011fd31dec3a9b507713a6c6509316fb6c2
Raihan Mistry <raihan@euphoriagenx.in>
2025-03-28 22:53:32 +0530

    req-req-28-03-2025

--- api/v1/urls.py
+++ api/v1/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from api.v1.views import BasicAPiView, ProjectApiView
 
+
+# faltu urls
 urlpatterns = [
     path('', BasicAPiView.as_view(), name="basic"),
     path('get/', ProjectApiView.as_view(), name="project"),
